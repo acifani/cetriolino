8ecf6bda83bc898877e193ec4f3941341fd51d4e
Tommaso A <tommaso.amici@me.com>
2018-11-17 13:28:15 +0100

    prettier

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,49 @@
-import * as fs from 'fs'
+import * as fs from "fs";
 
-type Key = string
-type Value = any
-type DB = { [key: string]: any }
+type Key = string;
+type Value = any;
+type DB = {
+  [key: string]: any;
+};
 
 export default class Cetriolino {
-    private db: DB = {}
+  private db: DB = {};
 
-    constructor(filePath: string) {
-        this.load(filePath)
-    }
+  constructor(filePath: string) {
+    this.load(filePath)
+  }
 
-    get(key: Key): Value {
-        return this.db[key]
-    }
+  get(key: Key): Value {
+    return this.db[key];
+  }
 
-    set(key: Key, value: any): DB { 
-        this.db[key] = value 
-        return this.db
-    }
+  set(key: Key, value: any): DB {
+    this.db[key] = value;
+    return this.db;
+  }
 
-    remove(key: Key): boolean {
-        return delete this.db[key]
-    }
+  remove(key: Key): boolean {
+    return delete this.db[key];
+  }
 
-    exists(key: Key): boolean {
-        return this.db[key] !== undefined
-    }
+  exists(key: Key): boolean {
+    return this.db[key] !== undefined;
+  }
 
-    keys(): Iterable<Key> {
-        return this.db.keys()
-    }
+  keys(): Iterable<Key> {
+    return this.db.keys();
+  }
 
-    clear(): void {
-        this.db = {}
-    }
+  clear(): void {
+    this.db = {};
+  }
 
-    private load(filePath: string) {
-        try {
-            const content = fs.readFileSync(filePath);
-            this.db = JSON.parse(content.toString()) || {}
-        } catch (e) {
-            throw e
-        }
+  private load(filePath: string) {
+    try {
+      const content = fs.readFileSync(filePath);
+      this.db = JSON.parse(content.toString()) || {};
+    } catch (e) {
+      throw e;
     }
+  }
 }
